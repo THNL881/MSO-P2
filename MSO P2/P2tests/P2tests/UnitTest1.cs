02792e6e86e720aee092e4396ea4c3978a13d91d
THNL881 <tinghangl@gmail.com>
2024-10-17 21:59:04 +0200

    added program.cs code, choosing presets and executing them

--- MSO P2/P2tests/P2tests/UnitTest1.cs
+++ MSO P2/P2tests/P2tests/UnitTest1.cs
@@ -4,20 +4,20 @@ namespace P2tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestTurnCommand()
-        {
-            //Arrange
+        //[Fact]
+        //public void TestTurnCommand()
+        //{
+        //    //Arrange
 
-            Character p1 = new Character(new System.Drawing.Point(0, 0), Direction.ViewDir.North);
-            TurnCommand turn = new TurnCommand("right");
+        //    Character p1 = new Character(new System.Drawing.Point(0, 0), Direction.ViewDir.North);
+        //    TurnCommand turn = new TurnCommand("right");
 
-            //act
-            turn.Execute(p1);
+        //    //act
+        //    turn.Execute(p1);
 
-            //assert
-            Assert.Equal(Direction.ViewDir.West, p1.direction);
+        //    //assert
+        //    Assert.Equal(Direction.ViewDir.West, p1.direction);
 
-        }
+        //}
     }
 }
--- MSO P2/Program.cs
+++ MSO P2/Program.cs
@@ -6,7 +6,7 @@ namespace MSO_P2
     {
         static void Main(string[] args)
         {
-            Character player = new Character(new Point(0, 0), Direction.ViewDir.East);
+            Character player = new Character(new Point(0, 0), Direction.ViewDir.North);
             Preset easyPreset = new Preset([new MoveCommand(10), new TurnCommand("right"),
                                             new MoveCommand(10), new TurnCommand("right"),
                                             new MoveCommand(10), new TurnCommand("right"),
@@ -16,23 +16,47 @@ namespace MSO_P2
                                             new MoveCommand(3), new TurnCommand("right"), new RepeatCommand([
                                                 new MoveCommand(1), new TurnCommand("right"), new RepeatCommand([new MoveCommand(2)], 5)
                                                 ], 3)]);
+            Console.WriteLine("choose option: preset or load file");
+            string? option = Console.ReadLine();
 
+            if (option != null && (option == "preset" || option == "Preset"))
+            {
+                Console.WriteLine("choose preset: easy, advanced or expert");
+                string? presetChoice = Console.ReadLine();
+
+                switch (presetChoice)
+                {
+                    case "easy": foreach (ICommand command in easyPreset.Commands)
+                        {
+                            command.Execute(player);
+                        }
+                        break;
+                    case "advanced": foreach (ICommand command in advancedPreset.Commands)
+                        {
+                            command.Execute(player);
+                        }
+                        break;
+                    case "expert": foreach (ICommand command in easyPreset.Commands)
+                        {
+                            command.Execute(player);
+                        }
+                        break;
+                    default: Console.WriteLine("invalid preset");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter the strategy you want to use and the filepath");
+                Console.WriteLine("strategies: TXTStrategy");
+                string[] valuables = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+                FileParser parser = new FileParser(valuables[0], valuables[1]);
+            }
+
+            Console.WriteLine("End state " + player.position.ToString() + " facing " + player.direction);
         }
 
-
-        //public void TestTurnCommand()
-        //{
-        //    //Arrange
-
-        //    Character p1 = new Character(new System.Drawing.Point(0, 0), Direction.ViewDir.North);
-        //    TurnCommand turn = new TurnCommand("right");
-
-        //    //act
-        //    turn.Execute(p1);
-
-        //    //assert
-        //    Assert.Equal(p1.direction, Direction.ViewDir.East);
-
-        //}
     }
 }
